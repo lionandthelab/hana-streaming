0058bd979dc384aba11c0b0fa64af2d0933b44a2
Lionandthelab <lionandthelab@gmail.com>
2021-11-06 03:14:50 +0900

    Apply youtube UI
    Signed-off-by: Lionandthelab <lionandthelab@gmail.com>

--- hana-stream/src/components/CompositionComponent.vue
+++ hana-stream/src/components/CompositionComponent.vue
@@ -1,57 +1,46 @@
 <template>
   <div>
     <p>{{ title }}</p>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
-      </li>
-    </ul>
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? "yes" : "no" }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
+    <q-video
+      :ratio="16 / 9"
+      src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"
+    ></q-video>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, computed, ref, toRef, Ref } from 'vue'
-import { Todo, Meta } from './models'
+import { defineComponent, PropType } from 'vue'
+import { Stream } from './models'
 
-function useClickCount () {
-  const clickCount = ref(0)
-  function increment () {
-    clickCount.value += 1
-    return clickCount.value
-  }
+// function useClickCount () {
+//   const clickCount = ref(0)
+//   function increment () {
+//     clickCount.value += 1
+//     return clickCount.value
+//   }
 
-  return { clickCount, increment }
-}
+//   return { clickCount, increment }
+// }
 
-function useDisplayTodo (todos: Ref<Todo[]>) {
-  const todoCount = computed(() => todos.value.length)
-  return { todoCount }
-}
+// function useDisplayTodo (todos: Ref<Todo[]>) {
+//   const todoCount = computed(() => todos.value.length)
+//   return { todoCount }
+// }
 
 export default defineComponent({
   name: 'CompositionComponent',
   props: {
-    title: {
-      type: String,
-      required: true
-    },
-    todos: {
-      type: Array as PropType<Todo[]>,
+    streams: {
+      type: Array as PropType<Stream[]>,
       default: () => []
     },
-    meta: {
-      type: Object as PropType<Meta>,
-      required: true
-    },
-    active: {
-      type: Boolean
+    title: {
+      type: String
     }
   },
-  setup (props) {
-    return { ...useClickCount(), ...useDisplayTodo(toRef(props, 'todos')) }
+  setup () {
+    // return { ...useClickCount(), ...useDisplayTodo(toRef(props, 'streams')) }
+    return {}
   }
 })
 </script>
